7bc8965faac8377ca7862ed84b1fce532c3ef476
Mustak9009 <91077142+Mustak9009@users.noreply.github.com>
2023-04-28 13:20:26 +0530

    clear code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
-
+import React from 'react';
+import {About,Contact,Experience,Feedbacks,Hero,Navbar,Tech,Works,StarsCanvas} from '../components';
 export default function Home() {
   return (
-   <h1>hi</h1>
+   <React.Fragment>
+      <div className='relative z-0 bg-primary'>
+        <div>
+          <Navbar/>
+          <Hero/>
+        </div>
+      </div>
+   </React.Fragment>
   )
 }
