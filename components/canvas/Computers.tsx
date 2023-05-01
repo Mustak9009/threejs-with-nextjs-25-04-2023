68b4010088b3aac4fe235c0210574df0954b5de3
Mustak9009 <91077142+Mustak9009@users.noreply.github.com>
2023-05-01 11:36:11 +0530

    add

--- components/canvas/Computers.tsx
+++ components/canvas/Computers.tsx
@@ -3,7 +3,6 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 const Computers = () => {
-  const computer = useGLTF("./public/desktop_pc/scene.gltf");
   return <h1>computers</h1>;
 };
 
